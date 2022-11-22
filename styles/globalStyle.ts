6cd68d00f5981d2e06545db95978fdf36f69c0dd
Daysemotion <kso88794@gmail.com>
2022-11-22 17:10:58 +0900

    [#2]🎨 style: GlobalFont 및 ColorTheme 설정

--- styles/globalStyle.ts
+++ styles/globalStyle.ts
@@ -2,34 +2,35 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle` 
-    ${reset}
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&family=Roboto:wght@300;400;500;700;900&display=swap');
+  ${reset}
 
-    a {
+  a {
     text-decoration: none;
       color: inherit;
-    }
-
-    *{
-        box-sizing: border-box;
-    }
-
-    input, textarea { 
-      -moz-user-select: auto;
-      -webkit-user-select: auto;
-      -ms-user-select: auto;
-      user-select: auto;
-    }
-    
-    input:focus {
-      outline: none;
-    }
-
-    button {
-      border: none;
-      background: none;
-      padding: 0;
-      cursor: pointer;
-    }
+  }
+  *{
+      box-sizing: border-box;
+      font-family: 'Noto Sans KR', sans-serif;
+  }
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+  input:focus {
+    outline: none;
+  }  
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyles;
---
+++ styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  dark: '#1E1E1E',
+  primary: '#454545',
+  secondary: '#757575',
+  tertiary: '#7D7D7D',
+  border: '#EBEBEB',
+  white: '#FFFFFF',
+  blue: '#3DA5F5',
+  blueLight: '#ECF6FE',
+  blueDark: '#3186C4',
+  green: '#22C58B',
+  red: '#F86D7D',
+};
+
+export type ColorsTypes = typeof colors;
+
+export const theme: DefaultTheme = {
+  colors,
+};
