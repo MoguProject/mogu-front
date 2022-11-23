bbaf0265f505e1fb8601af6dfe7405b18bee820b
Daysemotion <kso88794@gmail.com>
2022-11-24 04:45:31 +0900

    [#9]✨ feat: 데스크탑 모바일 프로젝트 카드 컴포넌트 구현 + 더미데이터 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,28 @@ import CardState from '../components/common/CardState';
 import CardTags from '../components/common/CardTags';
 import Image from 'next/image';
 import StudyDesktopCard from '../components/common/card/study/StudyDesktopCard';
+import ProjectMobileCard from '../components/common/card/project/ProjectMobileCard/ProjectMobileCard';
+
+export interface ProjectDummyData {
+  title: string;
+  categories: string[];
+  state: boolean;
+  totalUser: number;
+  currentUser: number;
+  like: number;
+  imgUrl: string;
+}
 
 const Home: NextPage = () => {
+  const ProjectDummyData: ProjectDummyData = {
+    title: 'React + Spring 스터디 / 프로젝트 모집 사이트 같이 만드실분',
+    categories: ['React', 'Typescript', 'MySQL', 'NodeJs', 'Spring'],
+    state: true,
+    totalUser: 4,
+    currentUser: 2,
+    like: 20,
+    imgUrl: '/images/dummy_image.png',
+  };
   return (
     <>
       <Header />
@@ -29,6 +49,8 @@ const Home: NextPage = () => {
         height={24}
       />
       <StudyDesktopCard state={true} likeState={true} />
+      {/* <ProjectDesktopCard data={ProjectDummyData} /> */}
+      <ProjectMobileCard data={ProjectDummyData} />
     </>
   );
 };
