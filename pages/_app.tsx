995552dfe05c10dc78dc5a47e5a1d5aa228b39e8
StarryPro <hansunguk6178@gmail.com>
2022-11-24 00:35:57 +0900

    [#11] ✨ feat: title, meta 태그 활용한 seo 최적화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/globalStyle';
 import { theme } from '../styles/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -11,6 +12,17 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
+        <Head>
+          <title>MoGu</title>
+          <meta
+            name="description"
+            content="스터디, 사이드 프로젝트 팀원을 구하는 가장 쉬운 방법. MoGu"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0,width=device-width"
+          />
+        </Head>
         <Component {...pageProps} />
         <ReactQueryDevtools />
       </ThemeProvider>
