6121c089e3c4393cebc33bba6fd23f818eac3c24
StarryPro <hansunguk6178@gmail.com>
2022-11-25 18:14:32 +0900

    ✨ [#17] feat : 프로젝트/스터디 컴포넌트 추가 및 프로젝트 더미 데이터 생성

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,52 +1,23 @@
 import { NextPage } from 'next';
-import { ProjectDummyData } from '..';
-import RegisterButton from '../../components/common/button/register';
 import ProjectDesktopCard from '../../components/common/card/project/ProjectDesktopCard';
-import FilteredBox from '../../components/common/FilteredBox';
 import Layout from '../../components/Layout';
-import {
-  HeaderWrapper,
-  HeaderStyled,
-  HeaderNavList,
-  ProjectsList,
-  FilterBoxWrapper,
-} from './styled';
+import { ProjectsList } from './styled';
+import { ProjectsData } from '../../dummy/ProjectsData';
+import ProjectHeader from '../../components/project-study/header';
+import { FilterBoxWrapper } from './styled';
+import FilteredBox from '../../components/common/FilteredBox';
 
 const ProjectsPage: NextPage = () => {
-  const ProjectDummyData: ProjectDummyData = {
-    title: 'React + Spring 스터디 / 프로젝트 모집 사이트 같이 만드실분',
-    categories: ['React', 'Typescript', 'MySQL', 'NodeJs', 'Spring'],
-    state: true,
-    totalUser: 4,
-    currentUser: 2,
-    like: 20,
-    imgUrl: '/images/dummy_image.png',
-  };
   return (
     <Layout>
-      <HeaderWrapper>
-        <HeaderStyled>
-          <nav>
-            <HeaderNavList>
-              <li>전체 프로젝트</li>
-              <li>전체 스터디</li>
-            </HeaderNavList>
-          </nav>
-          <RegisterButton>모집하기</RegisterButton>
-        </HeaderStyled>
-      </HeaderWrapper>
+      <ProjectHeader />
       <FilterBoxWrapper>
-        <FilteredBox></FilteredBox>
+        <FilteredBox />
       </FilterBoxWrapper>
       <ProjectsList>
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
-        <ProjectDesktopCard data={ProjectDummyData} />
+        {ProjectsData.map((data) => (
+          <ProjectDesktopCard data={data} />
+        ))}
       </ProjectsList>
     </Layout>
   );
--- pages/projects/styled.ts
+++ pages/projects/styled.ts
@@ -1,38 +1,5 @@
 import styled from 'styled-components';
 
-export const HeaderWrapper = styled.div`
-  width: 100%;
-  height: 68px;
-  color: ${(props) => props.theme.colors.primary};
-`;
-
-export const HeaderStyled = styled.div`
-  width: 100%;
-  height: 100%;
-  max-width: 1140px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 auto;
-  padding: 0 10px;
-`;
-
-export const HeaderNavList = styled.ul`
-  display: flex;
-
-  li {
-    padding: 0 24px;
-    font-size: 16px;
-    font-weight: 700;
-    color: ${(props) => props.theme.colors.primary};
-    cursor: pointer;
-
-    :nth-child(1) {
-      color: ${(props) => props.theme.colors.red};
-    } // 나중에 Active시 색상변경으로 업데이트 예정
-  }
-`;
-
 export const FilterBoxWrapper = styled.div`
   margin-bottom: 10px;
 `;
