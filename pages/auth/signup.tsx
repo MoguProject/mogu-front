af8dd8d2f06a569569694c2196004a4c5e727f8a
Daysemotion <kso88794@gmail.com>
2022-11-27 06:27:47 +0900

    ✨ [#18] feat : 로그인, 회원가입 페이지 구현 및 유효성검사 적용

---
+++ pages/auth/signup.tsx
@@ -0,0 +1,12 @@
+import Signup from '../../components/auth/Signup';
+import Layout from '../../components/Layout';
+
+const signup = () => {
+  return (
+    <Layout>
+      <Signup></Signup>
+    </Layout>
+  );
+};
+
+export default signup;
---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export interface LoginSubmitData {
+  email: string;
+  password: string;
+}
+
+export interface SignupSubmitData {
+  email: string;
+  password: string;
+  nickname: string;
+  password_check: string;
+  phone_number: number;
+}
