16ed57fd80634cd3bc47e88bfe950bde6f79d258
StarryPro <hansunguk6178@gmail.com>
2022-12-10 01:09:41 +0900

    :ambulance: hotfix: type 에러 처리

--- components/registration/cummunity/index.tsx
+++ components/registration/cummunity/index.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import 'react-quill/dist/quill.snow.css';
 import { RegistrationButton } from '../styled';
-import dynamic from 'next/dynamic';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import axios from 'axios';
+import { Container, ErrorMessage, PostRegistrationForm } from './styled';
+import Editor from '../Editor';
 
-const ReactQuill = dynamic(() => import('react-quill'), {
-  ssr: false,
-});
+export type FormValues = {
+  categoryId: string;
+  content: string;
+  title: string;
+};
 
 const CommunityPostRegistration = () => {
   const {
@@ -16,43 +19,40 @@ const CommunityPostRegistration = () => {
     setValue,
     trigger,
     formState: { errors },
-  } = useForm({
+  } = useForm<FormValues>({
     mode: 'onChange',
   });
-
   const [content, setContent] = useState('');
   const onChangeContents = (value: string) => {
     setContent(value);
     setValue('content', value);
     trigger('content');
   };
-  const onSubmit = async (data) => {
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
     console.log('data:', data);
     const formData = new FormData();
     formData.append('categoryId', data.categoryId);
     formData.append('content', data.content);
     formData.append('title', data.title);
-    console.log('formData:', formData);
-
-    await axios
-      .post('http://13.124.27.209:8080/posts/create', formData, {
+    try {
+      const response = await axios.post('/posts/create', formData, {
         headers: {
           'Content-Type': 'multipart/formdata',
         },
-      })
-      .then((response) => {
-        response.data;
-        console.log('response.data:', response.data);
-      })
-      .catch((error) => {
-        console.log(error);
       });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <>
+    <Container>
       <h1>글쓰기</h1>
-      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+      <PostRegistrationForm
+        onSubmit={handleSubmit(onSubmit)}
+        encType="multipart/form-data"
+      >
         <select {...register('categoryId')}>
           <option value={1}>팀 프로젝트</option>
           <option value={2}>개인 프로젝트</option>
@@ -63,11 +63,17 @@ const CommunityPostRegistration = () => {
           placeholder="제목을 작성해주세요."
           {...register('title', { required: true })}
         />
-        {errors.title && <p>제목을 작성해주세요.</p>}
-        <ReactQuill onChange={onChangeContents} value={content} />
+        {errors.title && <ErrorMessage>제목을 작성해주세요.</ErrorMessage>}
+        <Editor
+          onChange={onChangeContents}
+          value={content}
+          theme="snow"
+          placeholder="내용을 입력해주세요."
+          s
+        />
         <RegistrationButton>등록하기</RegistrationButton>
-      </form>
-    </>
+      </PostRegistrationForm>
+    </Container>
   );
 };
 
